ad4de773f2996ba941aec7673d5cbe9b3ddcdd02
jsots <justinsotolongo@gmail.com>
2023-05-23 10:43:01 -0400

    Snake head created and appearing in random locations

--- snake.py
+++ snake.py
@@ -2,6 +2,13 @@ import pygame as pg
 from random import randrange
 
 WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE -2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -10,6 +17,8 @@ while True:
         if event.type == pg.QUIT:
             exit()
     screen.fill('black')
+    # Draw snake below
+    [pg.draw.rect(screen, 'magenta', segment) for segment in segments]
     pg.display.flip()
     clock.tick(60)
 
